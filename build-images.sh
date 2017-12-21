4606dbc4271a4d3f043496473d45b263cb81909e
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2017-12-21 14:58:19 +0100

    Define integrated docker-compose

--- build-images.sh
+++ build-images.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# build image pesta/users
-../microservice-users/build-image.sh
+# build image pestakit/users
+cd ../microservice-users
+./build-image.sh
 
-# build image pesta/survey
-../microservice-survey/build-image.sh
+# build image pestakit/surveys
+cd ../microservice-survey
+./build-image.sh
 
-# bring topology up
-cd docker-topologies
-docker-compose up
